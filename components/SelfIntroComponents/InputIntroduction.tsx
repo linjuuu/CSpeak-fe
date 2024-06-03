f7f7df0ff20028d5a4c983cf48734c38cadeafc2
linjuuu <jun1_id@naver.com>
2024-06-03 19:58:49 +0900

    자기소개서 작성 페이지에서 키보드에 완료버튼 추가하여 내릴 수 있게 설정

--- components/SelfIntroComponents/InputIntroduction.tsx
+++ components/SelfIntroComponents/InputIntroduction.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import { View, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import FinishButton from './FinishButton';
 
 const InputIntroduction: React.FC = () => {
@@ -7,15 +7,30 @@ const InputIntroduction: React.FC = () => {
   const [introText , setIntroText] = useState('');
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+    >
       <View style={styles.inputContainer}>
-        <TextInput style={styles.titleInput} placeholder="자기소개서 문항 내용" onChangeText={(text) => setIntroTitle(text)}/>
+        <TextInput
+          style={styles.titleInput}
+          placeholder="자기소개서 문항 내용"
+          onChangeText={(text) => setIntroTitle(text)}
+          returnKeyType="done"
+          blurOnSubmit={true}
+        />
         <View style={styles.line} />
-        <TextInput style={styles.input} placeholder="자기소개를 입력해주세요" onChangeText={(text) => setIntroText(text)} multiline/>
+        <TextInput
+          style={styles.input}
+          placeholder="자기소개를 입력해주세요"
+          onChangeText={(text) => setIntroText(text)}
+          multiline
+          returnKeyType="done"
+          blurOnSubmit={true}
+        />
       </View>
       <FinishButton introTitle={introTitle} introText={introText} />
-    </View>
-    
+    </KeyboardAvoidingView>
   );
 };
 
@@ -25,6 +40,7 @@ const styles = StyleSheet.create({
     width: '100%',
     borderRadius: 10,
     padding: 20,
+    flex: 1,
   },
   inputContainer: {
     backgroundColor: 'rgba(255, 255, 255, 0.6)',
@@ -32,6 +48,7 @@ const styles = StyleSheet.create({
     height : '70%',
     borderRadius: 10,
     padding: 20,
+    flex: 1,
   },
   titleInput: {
     borderColor: 'lightgray',
@@ -53,7 +70,6 @@ const styles = StyleSheet.create({
     padding: 10,
     minHeight: 100,
   },
-  
 });
 
 export default InputIntroduction;
